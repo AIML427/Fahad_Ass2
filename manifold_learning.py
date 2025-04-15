89b9ce476b75a21f9a432c3fc951782fb75ebdd9
FAHAD <eng.bokhari@hotmail.com>
2025-04-15 21:33:42 +1200

    Construct the main function

---
+++ manifold_learning.py
@@ -0,0 +1,12 @@
+import numpy
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+def main():
+    x = numpy.random.rand(100, 3)
+
+
+if __name__ == "__main__":
+    main()
